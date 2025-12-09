8973a5e04771ffcea1eca338be4578602627e416
ieajohsila <50976723+ieajohsila@users.noreply.github.com>
2025-12-10 01:17:31 +0330

    Create create_admin.py

---
+++ core/management/commands/create_admin.py
@@ -0,0 +1,19 @@
+import os
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Creates a superuser if one does not exist'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        # --- مقادیر زیر را با اطلاعات دلخواه خودتان جایگزین کنید ---
+        ADMIN_USERNAME = 'admin'
+        ADMIN_EMAIL = 'admin@example.com'
+        ADMIN_PASSWORD = 'YourSuperStrongPassword123!' # <-- یک رمز عبور قوی انتخاب کنید
+
+        if not User.objects.filter(username=ADMIN_USERNAME).exists():
+            self.stdout.write(self.style.SUCCESS(f"Creating account for {ADMIN_USERNAME}"))
+            User.objects.create_superuser(ADMIN_USERNAME, ADMIN_EMAIL, ADMIN_PASSWORD)
+        else:
+            self.stdout.write(self.style.WARNING(f"Admin user '{ADMIN_USERNAME}' already exists. Skipping."))
